b853cc7fa3811e84778b14c1bc0d040256810f49
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-01-20 10:23:00 +0100

    Fix

--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,6 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-const options = {
-    params: {
-      bl_latitude: '11.847676',
-      tr_latitude: '12.838442',
-      bl_longitude: '109.095887',
-      tr_longitude: '109.149359',
-    },
-    headers: {
-      'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      'x-rapidapi-key': process.env.REACT_APP_TRAVEL_ADVISOR_API_KEY
-    }
-  };
-
 export const getPlacesData = async (sw, ne) => {
     try {
         const { data: { data } } = await axios.get(URL, {
@@ -26,7 +13,7 @@ export const getPlacesData = async (sw, ne) => {
             },
             headers: {
                 'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                'x-rapidapi-key': 'bfd4e35527mshe0ff1eb7dd5d1c8p108f43jsn7e06c566f987'
+                'x-rapidapi-key': process.env.REACT_APP_TRAVEL_ADVISOR_API_KEY
             }
         });
         
