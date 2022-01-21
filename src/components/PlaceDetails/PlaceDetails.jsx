eb4b9a9228bb2d8563c43af87f756e49a4ad8c81
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-01-21 09:00:02 +0100

    Update commit

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
-import { Rating } from '@material-ui/lab/Rating';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const PlaceDetails = ({ place }) => {
+const PlaceDetails = ({ place, selected, refProp }) => {
     const classes = useStyles();
+
+    if(selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" })
 
     return (
         <Card elevation={6}>
@@ -19,6 +21,10 @@ const PlaceDetails = ({ place }) => {
             <CardContent>
                 <Typography gutterBottom variant="h5">{place.name}</Typography>
                 <Box display="flex" justifyContent="space-between">
+                    <Rating value={Number(place.rating)} readOnly />
+                    <Typography gutterBottom variant='subtitle1'>out of {place.num_reviews}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
                     <Typography variant="subtitle1">Price</Typography>
                     <Typography gutterBottom variant='subtitle1'>{place.price}</Typography>
                 </Box>
@@ -26,8 +32,8 @@ const PlaceDetails = ({ place }) => {
                     <Typography variant="subtitle1">Ranking</Typography>
                     <Typography gutterBottom variant='subtitle1'>{place.ranking}</Typography>
                 </Box>
-                {place?.awards?.map((award) => (
-                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                {place?.awards?.map((award, i) => (
+                    <Box key={i} my={1} display="flex" justifyContent="space-between" alignItems="center">
                         <img src={award.images.small} alt={award.display_name} />
                         <Typography variant='subtitle2' color="textSecondary">{award.display_name}</Typography>
                     </Box>
